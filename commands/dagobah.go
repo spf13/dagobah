3653f450867418d925e638e9d20415ec3474116f
spf13 <steve.francia@gmail.com>
2014-06-26 20:05:25 -0400

    Now can fetch feeds

--- commands/dagobah.go
+++ commands/dagobah.go
@@ -8,6 +8,7 @@ package commands
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -15,7 +16,6 @@ import (
 
 var CfgFile string
 
-//var Config *hugolib.Config
 var RootCmd = &cobra.Command{
 	Use:   "dagobah",
 	Short: "Dagobah is an awesome planet style RSS aggregator",
@@ -43,12 +43,27 @@ func initConfig() {
 	viper.AddConfigPath("/etc/dagobah/")   // path to look for the config file in
 	viper.AddConfigPath("$HOME/.dagobah/") // call multiple times to add many search paths
 	viper.ReadInConfig()
+
+	viper.SetDefault("feeds", []map[string]string{{"Name": "Hacking Management", "Url": "url:http://spf13.com/index.xml"}})
+	viper.SetDefault("port", 1138)
+	viper.SetDefault("RSSTimeout", 5)
+
+	if fetchCmd.Flags().Lookup("rsstimeout").Changed {
+		viper.Set("RSSTimeout", RSSTimeout)
+	}
+}
+
+func addCommands() {
+	RootCmd.AddCommand(fetchCmd)
 }
 
 func Execute() {
+	addCommands()
 	err := RootCmd.Execute()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
+
+	time.Sleep(1 * time.Hour)
 }
---
+++ commands/fetch.go
@@ -0,0 +1,81 @@
+// Copyright © 2014 Steve Francia <spf@spf13.com>.
+//
+// Use of this source code is governed by an Apache2
+// license that can be found in the LICENSE file.
+
+package commands
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	rss "github.com/jteeuwen/go-pkg-rss"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var RSSTimeout int
+
+var fetchCmd = &cobra.Command{
+	Use:   "fetch",
+	Short: "Fetch feeds",
+	Long:  `Dagobah will fetch all feeds listed in the config file.`,
+}
+
+type ConfigFeed struct {
+	Name, Url string
+}
+
+type Config struct {
+	Feeds []ConfigFeed
+	Port  int
+}
+
+func init() {
+	fetchCmd.Flags().IntVar(&RSSTimeout, "rsstimeout", 5, "Timeout (in min) for RSS retrival")
+	fetchCmd.Run = fetchRun
+}
+
+func fetchRun(cmd *cobra.Command, args []string) {
+	initConfig()
+	Fetcher()
+}
+
+func Fetcher() {
+	var config Config
+	err := viper.GetAllIntoStruct(&config)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, feed := range config.Feeds {
+		go PollFeed(feed.Url)
+	}
+}
+
+func PollFeed(uri string) {
+	timeout := viper.GetInt("RSSTimeout")
+	if timeout < 1 {
+		timeout = 1
+	}
+	feed := rss.New(timeout, true, chanHandler, itemHandler)
+
+	for {
+		if err := feed.Fetch(uri, nil); err != nil {
+			fmt.Fprintf(os.Stderr, "[e] %s: %s", uri, err)
+			return
+		}
+
+		fmt.Printf("Sleeping for %d seconds on %s\n", feed.SecondsTillUpdate(), uri)
+		<-time.After(time.Duration(feed.SecondsTillUpdate() * 1e9))
+	}
+}
+
+func chanHandler(feed *rss.Feed, newchannels []*rss.Channel) {
+	fmt.Printf("%d new channel(s) in %s\n", len(newchannels), feed.Url)
+}
+
+func itemHandler(feed *rss.Feed, ch *rss.Channel, newitems []*rss.Item) {
+	fmt.Printf("%d new item(s) in %s\n", len(newitems), feed.Url)
+}
